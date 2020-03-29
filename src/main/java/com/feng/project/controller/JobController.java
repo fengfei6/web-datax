9a8edd799d870a3b089d3ad8f08d68aa068bea6f
fengfei6 <fengfei6@yonyou.com>
2020-03-29 21:26:44 +0800

    修改jobUtil中生成json文件的格式

--- src/main/java/com/feng/project/controller/JobController.java
+++ src/main/java/com/feng/project/controller/JobController.java
@@ -4,14 +4,21 @@ import com.feng.project.domain.Job;
 import com.feng.project.domain.User;
 import com.feng.project.service.DatasourceService;
 import com.feng.project.service.JobService;
+import com.feng.project.util.JobUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class JobController {
@@ -43,5 +50,16 @@ public class JobController {
     public ModelAndView findAllJob(Model model){
         model.addAttribute("joblist",jobService.findAll());
         return new ModelAndView("admin/job-list","model",model);
+    }
+
+    @RequestMapping("/job/execJob")
+    public List<String> doJobOnce(Integer id) throws IOException {
+        Job job = jobService.getOne(id);
+        JobUtil.getJsonfile(datasourceService.getDatasource(
+                job.getReaderDbId()),datasourceService.getDatasource(job.getWriterDbId()),
+                job.getReaderTable(),job.getWriterTable(),job.getName());
+        List<String> list = new ArrayList<>();
+        list.add("succ");
+        return list;
     }
 }
---
+++ src/main/java/com/feng/project/util/JobUtil.java
@@ -0,0 +1,64 @@
+package com.feng.project.util;
+
+
+import com.feng.project.domain.Datasource;
+import com.feng.project.service.DatasourceService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+public class JobUtil {
+
+	public static void getJsonfile(Datasource sdatasource,Datasource tdatasource,String stable,String ttable,String jobName) throws IOException {
+		String reader= null;
+		String sjdbcUrl= null;
+
+		if("mysql".equalsIgnoreCase(sdatasource.getType())){
+			reader = "mysqlreader";
+			sjdbcUrl = Constants.MYSQL_JDBC_NAME+sdatasource.getIp()+":"+sdatasource.getPort()+"/"+sdatasource.getDbname();
+		}else if("oracle".equalsIgnoreCase(sdatasource.getType())){
+			reader = "oraclereader";
+			sjdbcUrl = Constants.ORACLE_THIN_JDBC_NAME+sdatasource.getIp()+":"+sdatasource.getPort()+"/"+sdatasource.getDbname();
+		}
+
+		String writer= null;
+		String tjdbcUrl= null;
+		if("mysql".equalsIgnoreCase(tdatasource.getType())){
+			writer = "mysqlwriter";
+			tjdbcUrl = Constants.MYSQL_JDBC_NAME+sdatasource.getIp()+":"+sdatasource.getPort()+"/"+sdatasource.getDbname();
+		}else if("oracle".equalsIgnoreCase(tdatasource.getType())){
+			writer = "oraclewriter";
+			tjdbcUrl = Constants.ORACLE_THIN_JDBC_NAME+sdatasource.getIp()+":"+sdatasource.getPort()+"/"+sdatasource.getDbname();
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append("{\"job\": {\"setting\": {\"speed\": {\"channel\": 3},\"errorLimit\": {\"record\": 0,\"percentage\": 0.02}}," +
+				"\"content\": [{ \"reader\":{\"name\": \"").append(reader);
+		sb.append("\",\"parameter\":{\"username\":\"").append(sdatasource.getUsername());
+		sb.append("\",\"password\":\"").append(sdatasource.getPassword());
+		sb.append("\",\"column\": [\"*\"],");
+		sb.append("\"connection\": [{\"table\": [\"").append(stable);
+		sb.append("\"],\"jdbcUrl\": [\"").append(sjdbcUrl);
+		sb.append("\"]}]}},");
+		sb.append("\"writer\": {\"name\": \"").append(writer);
+		sb.append("\",\"parameter\":{\"username\":\"").append(tdatasource.getUsername());
+		sb.append("\",\"password\":\"").append(tdatasource.getPassword());
+		sb.append("\",\"column\": [\"*\"],");
+		sb.append("\"connection\": [{\"table\": [\"").append(ttable);
+		sb.append("\"],\"jdbcUrl\": [\"").append(tjdbcUrl);
+		sb.append("\"]}]}}}]}}");
+	
+		File file = new File("src/main/resources/static/file/"+jobName+".json");
+		if(!file.exists()) {
+			file.createNewFile();
+		}
+		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+		bos.write(sb.toString().getBytes());
+		bos.close();
+	}
+}
