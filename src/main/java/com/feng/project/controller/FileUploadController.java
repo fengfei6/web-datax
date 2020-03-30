09d9b2d5f5766cf6e81f9acc77155998bdc8f4a0
fengfei6 <fengfei6@yonyou.com>
2020-03-30 14:30:43 +0800

    job-file交互修改

--- src/main/java/com/feng/project/controller/FileUploadController.java
+++ src/main/java/com/feng/project/controller/FileUploadController.java
@@ -1,48 +1,57 @@
 package com.feng.project.controller;
 
-import ch.ethz.ssh2.Connection;
-import com.feng.project.util.DataxUtil;
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.Trigger;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.multipart.MultipartFile;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.feng.project.util.DataxUtil;
+
+import ch.ethz.ssh2.Connection;
 
 @Controller
 public class FileUploadController {
     // 项目根路径下的目录  -- SpringBoot static 目录相当于是根路径下（SpringBoot 默认）
     public final static String UPLOAD_PATH_PREFIX = "static/file/";
+    
+    @ResponseBody
     @PostMapping("/upload")
-    public String upload(MultipartFile uploadFile, HttpServletRequest request, Model model) throws IOException {
-        if(uploadFile.isEmpty()){
-            return "请选择上传文件";
+    public List<String> upload(MultipartFile files) throws IOException {
+    	List<String> list = new ArrayList<>();
+    	if(files.isEmpty()){
+            return list;
         }
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd/");
         String realPath = new String("src/main/resources/" + UPLOAD_PATH_PREFIX);
-        String format = sdf.format(new Date());
-        File file = new File(realPath + format);
-        if(!file.isDirectory()){
-            //递归生成文件夹
-            file.mkdirs();
-        }
-        String name = uploadFile.getOriginalFilename();
+        File file = new File(realPath);
+        String name = files.getOriginalFilename();
         //构建真实的文件路径
         File newFile = new File(file.getAbsolutePath() + File.separator + name);
         //转存文件到指定路径，如果文件名重复的话，将会覆盖掉之前的文件,这里是把文件上传到 “绝对路径”
-        uploadFile.transferTo(newFile);
-        model.addAttribute("file",newFile.getPath());
-        return "admin/exec";
+        files.transferTo(newFile);
+        
+        FileReader fr = new FileReader("src/main/resources/static/file/"+name); 
+    	StringBuilder sb = new StringBuilder();
+    	int ch = 0;  
+    	while((ch = fr.read())!=-1 ){   
+    		sb.append((char)ch);   
+    	} 
+    	list.add(sb.toString());
+    	return list;
     }
-
 
     @PostMapping("/exec")
     public String dataxService(String filepath) throws IOException {
